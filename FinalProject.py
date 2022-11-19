70026ed1a1441674cae85003fa3c1fe0cafbbd17
salonigupta9 <117244589+salonigupta9@users.noreply.github.com>
2022-11-18 21:03:05 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,3 +1,21 @@
-#hello
-"""this function is a class"""
+
 class GuessingGame: 
+  """ This is the main class for our program. It will store different 
+  functions""" 
+  
+ 
+  def parse_args(argslist): 
+    """ Parse command line arguments. 
+        Expect 5 mandatory arguments: 
+        str: path to a text file containing ethnicity entered by users 
+        str: path to a text file containing gender entered by users 
+        int: path to a text file containing age entered by users 
+        str: path to a text file containing answers to a specific question
+        regarding a specific actor from the user input 
+        str: path to a text file containing answers to a specific question 
+        regarding a specific actor from the user input
+        Args: 
+        argslist (list of str/int): arguments from the command line
+        Returns: 
+        
+        
